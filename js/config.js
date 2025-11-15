07aecdee468ba901e01d12c0755d881f9ff9e5f2
manoj009876540987654 <144827758+manoj009876540987654@users.noreply.github.com>
2025-11-15 18:43:18 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_HnOdkMQ2j', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'bt0cukspd0244b2bndc5gkj4b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'ap-south-1_qVuTuRrZL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6t7660i7cng2gpui3k72l6vukr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://vpdv0dwyeh.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://x3w4j0cqii.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
